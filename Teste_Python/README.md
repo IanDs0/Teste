a93e82f80d23360e8390692456692a5a3c9f16b8
IanLLHonório <62866446+IanDs0@users.noreply.github.com>
2022-12-19 13:50:17 -0300

    Add files via upload

---
+++ Teste_Python/README.md
@@ -0,0 +1,17 @@
+## Instalar as bibliotecas utilizadas
+
+`pip install -r requirements.txt`
+
+## Comando para executar cada um dos arquivos .py
+
+### Executa o arquivo que pega os contatos e salva em um '.csv'
+
+`python pegarcontatos.py`
+
+### Executa o sistema de envio de mensagens que pega os contatos e salvos e procura o padrão de grupos ou contato que estão em um '.csv' que deve ser colocado
+
+`python enviomensagem.py`
+
+### Fazer o Arquivo '.exe'
+
+`python -m PyInstaller --onefile -w arquivo_desejado.py`
---
+++ Teste_Python/pegarcontatos.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+
+from time import sleep
+import sys
+import pandas as pd
+
+import os
+from selenium import webdriver
+
+dir_path = os.getcwd()
+profile = os.path.join(dir_path, "profile", "wpp")
+options = webdriver.ChromeOptions()
+options.add_argument(r"user-data-dir={}".format(profile))
+
+driver = webdriver.Chrome(chrome_options=options)
+
+driver.get("https://web.whatsapp.com")
+
+while len(driver.find_elements(By.ID, 'side')) < 1:
+    sleep(2)
+sleep(2)
+
+iframe = driver.find_element(By.XPATH, '//*[@id="pane-side"]/div/div/div')
+altura = 700
+scroll_origin = ScrollOrigin.from_element(iframe)
+
+sleep(1)
+
+Contato = []
+temporario = []
+anterior = []
+
+while(True):
+
+    sleep(0.1)
+
+    # Pega os contatos
+    for person in driver.find_elements(By.XPATH,'//*[@id="pane-side"]/div/div/div/div'):
+        nomeContato = person.find_element(By.XPATH, './div/div/div/div[2]/div[1]/div').text
+        Contato.append(nomeContato)
+        temporario.append(nomeContato)
+    
+    # Anda para baixo
+    ActionChains(driver)\
+        .scroll_from_origin(scroll_origin, 0, altura)\
+        .perform()
+    
+    if(anterior == temporario):
+        break
+
+    anterior=temporario.copy()
+    temporario.clear()
+
+teste = list(set(Contato))
+
+#salva
+salvar = pd.DataFrame(teste)
+salvar = salvar.rename(columns = {0:'Contatos'})
+salvar.to_csv("Contatos.csv")
+
+#leitura e filtragem
+# salvo = pd.read_csv("./Contatos.csv")
+# salvo = salvo['Contatos'].values
+
+# Busca = ['Pedro','Teste','Gelson']
+# envio = []
+
+# for busca in Busca:
+#     for contact in salvo:
+#         if contact.startswith(busca):
+#             envio.append(contact)
+
+driver.quit()
